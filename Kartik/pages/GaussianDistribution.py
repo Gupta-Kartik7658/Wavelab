e7a8fbfa08117ea66068e8d36e764c401f7c74f9
Gupta-Kartik-IIITV <202351056@iiitvadodara.ac.in>
2024-11-27 23:03:30 +0530

    plotly

--- Kartik/pages/GaussianDistribution.py
+++ Kartik/pages/GaussianDistribution.py
@@ -44,24 +44,7 @@ def gaussian_distribution(col1,col2,mu=0, sigma=1, N=2000000,xlim=5):
         showlegend=False,
         xaxis_range=[-xlim, xlim],           
     ) 
-
-   
-    # ax1.hist(Z, bins=100, histtype="stepfilled", density="True", range=[-xlim, xlim])
-    # ax1.set_xlabel('X')
-    # ax1.set_ylabel('$f_x(x)$')
-    # ax1.set_title('Standard Gaussian Distribution PDF')
-    # ax1.grid(True)  # Add grid to the PDF plot
-    # ax1.set_facecolor('#f7f7f7')  # Set background color
     
-    
-    ax2.hist(Z, bins=100, cumulative=True, density=True, alpha=0.6, color='g', range=[-xlim, xlim])
-    ax2.set_xlabel('X')
-    ax2.set_ylabel('$F_x(x)$')
-    ax2.set_title('Standard Gaussian Distribution CDF')
-    ax2.grid(True)  
-    ax2.set_facecolor('#f7f7f7')  
-
-   
     with col1:
         st.plotly_chart(fig1)
     with col2:
