2e6d07d23affc6129333939255281db4622e39c4
Gupta-Kartik7658 <kartik.gupta7658@gmail.com>
2025-02-10 23:27:48 +0530

    Remove MatLAb codes

--- MATLAB codes/MATLAB For MA/Covariance.m
+++
@@ -1,6 +0,0 @@
-function Cx = Covariance(X)
-    [m, n] = size(X);
-    mu = mean(X);
-    X_c = X - mu;
-    Cx = (1/(m-1)) .* (X_c' * X_c);
-end
--- MATLAB codes/MATLAB For MA/imageDistribution.m
+++
@@ -1,7 +0,0 @@
-function X = imageDistribution(path)
-    img = imread(path);
-    img = imresize(img,[512,512]);
-    img = rgb2gray(img);
-    X = img(:);
-end
-
